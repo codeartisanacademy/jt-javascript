34e74d22d2875f1c858cb22659192f25aa8d9580
wahyudi <wahyudi.coding@gmail.com>
2021-05-22 13:41:34 +0700

    test

--- recovery.js
+++ recovery.js
@@ -2,8 +2,17 @@ if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(onSuccess, onFailure);
 }  
 
+let form ;
+
+// create global var for map
+
+// create global var for map event
+
 function onSuccess(position){
     const mymap = L.map('map').setView([position.coords.latitude, position.coords.longitude], 15);
+
+    const formContainer = document.getElementById('form-container');
+    
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 18,
@@ -15,8 +24,23 @@ function onSuccess(position){
     
         const marker = L.marker([position.coords.latitude, position.coords.longitude], {title:"You are here"}).addTo(mymap);
         marker.bindPopup("You are here").openPopup();
+
+        mymap.on('click', function(e){
+            L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap).bindPopup("hello").openPopup();
+            formContainer.classList.remove('d-none');
+            formContainer.classList.add('bg-light');
+            form = document.getElementById("mapform");
+        });
+        
+        
 }
 
 function onFailure(){
     alert("Your browser doesn't support geo location");
 }
+
+form.addEventListener('submit', function(e){
+    // add marker etc
+    e.preventDefault();
+    console.log('submitted');
+})
